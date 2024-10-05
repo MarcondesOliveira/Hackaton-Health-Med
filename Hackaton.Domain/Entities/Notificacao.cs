62d5862644f5c73295e7b48063bfc4d06e9f7d2e
MarcondesOliveira <marcondescurso@outlook.com>
2024-10-05 04:40:36 -0300

    e-mail SMTP configurado

---
+++ Hackaton.Domain/Entities/Notificacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton.Domain.Entities
+{
+    public class Notificacao
+    {
+        public string Mensagem { get; set; }
+        //public Tipo Tipo { get; set; }
+        public string Destinatario { get; set; }
+    }
+}
